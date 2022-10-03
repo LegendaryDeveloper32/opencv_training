1a8d731f46dd11e37c80bb4fa3c216c558228f74
LegendaryDeveloper32 <legendarydeveloper32@outlook.com>
2022-10-04 04:42:23 +0800

    Video read and write

---
+++ video_read_write/video_read_write.py
@@ -0,0 +1,25 @@
+import cv2
+vid_capture = cv2.VideoCapture("city.mp4")
+
+if(vid_capture.isOpened() == False):
+    print("Error Opening the video file")
+else:
+    fps = vid_capture.get(5)
+    print('Frames per second : ', fps, 'FPS')
+
+    frame_count = vid_capture.get(7)
+    print("Frame count : ", frame_count)
+
+while(vid_capture.isOpened()):
+    ret, frame = vid_capture.read()
+    if ret == True:
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(10)
+
+        if key == ord('q'):
+            break;
+    else:
+        break;
+
+vid_capture.release()
+cv2.destroyAllWindows()
